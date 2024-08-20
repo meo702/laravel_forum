9e71dea3e1a901ba552bd2944c355a9503a23ad8
Rinat Ilyasov <87395162+yipeeh@users.noreply.github.com>
2024-08-21 01:12:05 +0500

    public message

--- resources/js/Pages/Theme/Show.vue
+++ resources/js/Pages/Theme/Show.vue
@@ -4,7 +4,21 @@
             <h3 class="text-xl mr-4">{{ theme.title }}</h3>
         </div>
         <div>
-            
+
+        </div>
+
+        <div class="bg-white border border-gray-300 p-4">
+            <div class="mb-4">
+                <h3 class="text-xl mr-4">Добавить сообщение</h3>
+            </div>
+            <div class="mb-4">
+                <div ref="editor" contenteditable="true" class="w-full border border-gray-300 p-2">
+
+                </div>
+            </div>
+            <div>
+                <a @click.prevent="store" href="#" class="block w-1/4 p-2 bg-sky-600 text-white text-center border border-sky-700">Опубликовать</a>
+            </div>
         </div>
     </div>
 </template>
@@ -20,11 +34,25 @@ import { Link } from '@inertiajs/vue3';
         ],
 
         data() {
-
+            return {
+                // content: '',
+            }
         },
 
         components: {
             Link
+        },
+
+        methods: {
+            store() {
+                axios.post('/messages', {
+                    content: this.$refs.editor.innerHTML,
+                    theme_id: this.theme.id
+                }).then( res => {
+                    console.log(res)
+                    this.$refs.editor.innerHTML = ''
+                })
+            }
         },
 
         layout: MainLayot
